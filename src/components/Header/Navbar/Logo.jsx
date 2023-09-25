3b3a4f36b2b33687bf1adf52e8ffd6ee6c69bdee
e <jannatul0040@gmail.com>
2023-09-25 15:31:41 +0600

    created many component and design a navbar section

---
+++ src/components/Header/Navbar/Logo.jsx
@@ -0,0 +1,11 @@
+
+
+const Logo = () => {
+    return (
+        <div>
+            <img className="w-3/4" src="/Logo.png" alt="" />
+        </div>
+    );
+};
+
+export default Logo;
---
+++ src/components/Header/Navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import { NavLink } from "react-router-dom";
+import Logo from "./Logo";
+
+const Navbar = () => {
+    return (
+        <div className="">
+            <nav className="flex justify-between items-center py-5 px-5">
+                <Logo></Logo>
+                <ul className="flex gap-5">
+                    <li>
+                        <NavLink
+                            to="/"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending" : isActive ? "text-[#FF444A] underline font-bold text-lg" : ""
+                            }
+                        >
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/donation"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending" : isActive ? "text-[#FF444A] underline font-bold text-lg" : ""
+                            }
+                        >
+                            Donation
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/statistics"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending" : isActive ? "text-[#FF444A] underline font-bold text-lg" : ""
+                            }
+                        >
+                            Statistics
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
